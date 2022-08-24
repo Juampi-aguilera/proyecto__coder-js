44afc66b6c29c5d7ef49275ab7f4609395378833
juan aguilera <juampiag3@gmail.com>
2022-08-24 07:23:13 -0300

    carrito al storage

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@
 let servicios = [];
 
 // carrito de compras
-let carrito=0;
 let textoCarrito=document.createElement("h3");
 let contenedorCarrito=document.querySelector("#carrito");
+
+//funcionalidad para el carrito
+let borrarCarrito=document.createElement("button");
+
+//agregando datos del carrito al storage
+carrito=JSON.parse(localStorage.getItem("carrito"));
 
 //botones de agregar al carrito
 const sesionX1 = document.querySelector("#agregar__sesionX1");
@@ -27,23 +32,40 @@ let servicio3=new Servicio("Pack de Ocho Sesiones","Sesion",15200);
 //pusheamos los servicios al array
 servicios.push(servicio1,servicio2,servicio3);
 
-// funcion flecha para agregar un producto al carrito
-const agregarAlCarrito = (producto) => {
-    carrito+=producto.precio;// hacer funcion de esto
-    textoCarrito.textContent=`El total a pagar es de $${carrito}`;
+// si hay algo en el carrito este se muestra
+const actualizarCarrito=_=>{
     if(carrito>0){
         textoCarrito.className="text-center m-5 border border-secondary";
         contenedorCarrito.append(textoCarrito);
+        contenedorCarrito.append(borrarCarrito);
+        textoCarrito.textContent=`El total a pagar es de $${carrito}`;
     }
+}
+actualizarCarrito();
+
+// funcion flecha para agregar un producto al carrito
+const agregarAlCarrito = (producto) => {
+    carrito+=producto.precio;
+    actualizarCarrito();
+    localStorage.setItem("carrito",carrito);
 }
 
 // cuando se toca el boton se agrega al carrito
 sesionX1.addEventListener("click",function(){
     agregarAlCarrito(servicio1);
-})
+}) 
 sesionX4.addEventListener("click",function(){
     agregarAlCarrito(servicio2);
 })
 sesionX8.addEventListener("click",function(){
     agregarAlCarrito(servicio3);
+})
+
+//eliminar carrito
+borrarCarrito.textContent="Eliminar carrito";
+borrarCarrito.className="btn btn-outline-success mx-5";
+
+borrarCarrito.addEventListener("click",function(){
+    carrito=0;
+    localStorage.setItem("carrito",carrito);
 })
